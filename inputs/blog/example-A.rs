08285960bd330d1a4a36ce0c8e4343aa74053455
Julien Cretin <cretin@google.com>
2018-05-28 16:48:55 +0200

    Add blog example A with nll-facts

---
+++ inputs/blog/example-A.rs
@@ -0,0 +1,13 @@
+#![feature(nll)]
+
+fn take<T>(_p: T) {}
+
+fn main() {
+    let mut x: i32 = 22;
+    let mut v: Vec<&i32> = vec![];
+    let r: &mut Vec<&i32> = &mut v;
+    let p: &i32 = &x;
+    r.push(p);
+    x += 1;
+    take(v);
+}
